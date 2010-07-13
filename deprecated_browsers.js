a12c5b317d98dc8a1042103f01f335d6a091efb8
brunomarks@gmail.com <bruno.marques@bruno-marques.local>
2010-07-13 10:36:37 -0300

    update code *pngfix function

--- deprecated_browsers.js
+++ deprecated_browsers.js
@@ -1,15 +1,15 @@
-	/**
-	* Author: Bruno Marques - MKT VIRTUAL
-	* Email: bruno@mktvirtual.com.br
-	* URL: http://github.com/brunomarks/updatebrowser
-	* Version: 1.0
-	* Licensed under the MIT License
-	* usage: include js - http://github.com/mktvirtual/Deprecated-Browser/raw/multi-language/deprecated_browsers.js
-	* call function deprecated to init, - deprecated('color'); where color = hexadecimal string;
-	**/	
+/**
+* Author: Bruno Marques - MKT VIRTUAL
+* Email: bruno@mktvirtual.com.br
+* URL: http://github.com/brunomarks/updatebrowser
+* Version: 1.0
+* Licensed under the MIT License
+* usage: include js - http://github.com/mktvirtual/Deprecated-Browser/raw/multi-language/deprecated_browsers.js
+* call function deprecated to init, - deprecated('color'); where color = hexadecimal string;
+**/	
 var language;
 var deprecated_browser = {
-		
+
 	closeDeprecated: function(){
 		document.getElementById("deprecated_browser").style.display = 'none';
 		deprecated_browser.setCookie('deprecated_browser','jabulani',2);
@@ -25,23 +25,23 @@ var deprecated_browser = {
 
 		var about_deprecated_browser  = document.createElement('div');	
 		about_deprecated_browser.style.backgroundColor = colorBg;
-			document.body.prependChild = function(newChild) {
-				this.insertBefore(newChild, this.firstChild);
-				};
+		document.body.prependChild = function(newChild) {
+			this.insertBefore(newChild, this.firstChild);
+		};
 
-			//arr.push({title_intro:title_intro, desc_intro:desc_intro, mooreinfo:mooreinfo, title_splash:title_splash, description_splash:description_splash, invite_splash:invite_splash});	
-			document.body.prependChild(about_deprecated_browser);	
-			about_deprecated_browser.setAttribute('id', 'about_deprecated_browser');
-			about_deprecated_browser.innerHTML = "<div id='content_deprecated_browser'><a href='#' onClick='deprecated_browser.closeFancy()' id='close_about_deprecated_browser'><img src='http://github.com/mktvirtual/Deprecated-Browser/raw/multi-language/images/xis.png' alt='Fechar' /></a><p class='tit_deprecated_browser'>" + arrLanguage[0].title_splash + "</p><p id='description_deprecated_browser'>"+ arrLanguage[0].description_splash +"</p><p class='tit_deprecated_browser'>"+ arrLanguage[0].invite_splash +"</p><ul><li><a target='blank' href='http://br.mozdev.org'><img src='http://github.com/mktvirtual/Deprecated-Browser/raw/multi-language/images/firefox_b.png' alt='Download Firefox' /></a></li><li><a target='blank' href='http://www.microsoft.com/brasil/windows/internet-explorer'><img src='http://github.com/mktvirtual/Deprecated-Browser/raw/multi-language/images/ie_b.png' alt='Download Internet Explorer' /></a></li><li><a target='blank' href='http://www.google.com.br/chrome'><img src='http://github.com/mktvirtual/Deprecated-Browser/raw/multi-language/images/chrome_b.png' alt='Download Chrome' /></a></li>						<li><a target='blank' href='http://www.apple.com/br/safari'><img src='http://github.com/mktvirtual/Deprecated-Browser/raw/multi-language/images/safari_b.png' alt='Download Safari' /></a></li></ul></div>"
+		//arr.push({title_intro:title_intro, desc_intro:desc_intro, mooreinfo:mooreinfo, title_splash:title_splash, description_splash:description_splash, invite_splash:invite_splash});	
+		document.body.prependChild(about_deprecated_browser);	
+		about_deprecated_browser.setAttribute('id', 'about_deprecated_browser');
+		about_deprecated_browser.innerHTML = "<div id='content_deprecated_browser'><a href='#' onClick='deprecated_browser.closeFancy()' id='close_about_deprecated_browser'><img src='http://github.com/mktvirtual/Deprecated-Browser/raw/multi-language/images/xis.png' alt='Fechar' /></a><p class='tit_deprecated_browser'>" + arrLanguage[0].title_splash + "</p><p id='description_deprecated_browser'>"+ arrLanguage[0].description_splash +"</p><p class='tit_deprecated_browser'>"+ arrLanguage[0].invite_splash +"</p><ul><li><a target='blank' href='http://br.mozdev.org'><img src='http://github.com/mktvirtual/Deprecated-Browser/raw/multi-language/images/firefox_b.png' alt='Download Firefox' /></a></li><li><a target='blank' href='http://www.microsoft.com/brasil/windows/internet-explorer'><img src='http://github.com/mktvirtual/Deprecated-Browser/raw/multi-language/images/ie_b.png' alt='Download Internet Explorer' /></a></li><li><a target='blank' href='http://www.google.com.br/chrome'><img src='http://github.com/mktvirtual/Deprecated-Browser/raw/multi-language/images/chrome_b.png' alt='Download Chrome' /></a></li>						<li><a target='blank' href='http://www.apple.com/br/safari'><img src='http://github.com/mktvirtual/Deprecated-Browser/raw/multi-language/images/safari_b.png' alt='Download Safari' /></a></li></ul></div>"
 
-			document.getElementById("about_deprecated_browser").style.backgroundColor = colorBg;
-			var box_deprecated_browser = document.getElementById("content_deprecated_browser").getElementsByTagName('p');				
-			for (var i = 0; i <= box_deprecated_browser.length; i++){
-				var k = document.getElementById("content_deprecated_browser").childNodes[i];
-				k.style.color = colorText;
-			};
-			var img = document.getElementById("content_deprecated_browser").getElementsByTagName('img');
-			this.fix(img);
+		document.getElementById("about_deprecated_browser").style.backgroundColor = colorBg;
+		var box_deprecated_browser = document.getElementById("content_deprecated_browser").getElementsByTagName('p');				
+		for (var i = 0; i <= box_deprecated_browser.length; i++){
+			var k = document.getElementById("content_deprecated_browser").childNodes[i];
+			k.style.color = colorText;
+		};
+		var img = document.getElementById("content_deprecated_browser").getElementsByTagName('img');
+		this.fix(img);
 	},
 	getCookie:function(c_name){
 		if (document.cookie.length>0){
@@ -78,68 +78,68 @@ var deprecated_browser = {
 		var version = parseFloat(arVersion[1]);			
 		if ((version >= 5.5) && (document.body.filters)) 
 		{
-		   for(var i=0; i<imagens.length; i++)
-		   {
+			for(var i=0; i<imagens.length; i++)
+			{
 				var img = imagens[i];
-   			      var imgName = img.src.toUpperCase()
-   			      if (imgName.substring(imgName.length-3, imgName.length) == "PNG")
-   			      {
-   			         var imgID = (img.id) ? "id='" + img.id + "' " : ""
-   			         var imgClass = (img.className) ? "class='" + img.className + "' " : ""
-   			         var imgTitle = (img.title) ? "title='" + img.title + "' " : "title='" + img.alt + "' "
-   			         var imgStyle = "display:inline-block;" + img.style.cssText 
-   			         if (img.align == "left") imgStyle = "float:left;" + imgStyle
-   			         if (img.align == "right") imgStyle = "float:right;" + imgStyle
-   			         if (img.parentElement.href) imgStyle = "cursor:hand;" + imgStyle
-   			         var strNewHTML = "<span " + imgID + imgClass + imgTitle
-   			         + " style=\"" + "width:" + img.width + "px; height:" + img.height + "px;" + imgStyle + ";"
-   			         + "filter:progid:DXImageTransform.Microsoft.AlphaImageLoader"
-   			         + "(src=\'" + img.src + "\', sizingMethod='scale');\"></span>" 
-   			         img.outerHTML = strNewHTML
-   			         i = i-1
-   			      }
-   			   }
+				var imgName = img.src.toUpperCase()
+				if (imgName.substring(imgName.length-3, imgName.length) == "PNG")
+				{
+					var imgID = (img.id) ? "id='" + img.id + "' " : ""
+					var imgClass = (img.className) ? "class='" + img.className + "' " : ""
+					var imgTitle = (img.title) ? "title='" + img.title + "' " : "title='" + img.alt + "' "
+					var imgStyle = "display:inline-block;" + img.style.cssText 
+					if (img.align == "left") imgStyle = "float:left;" + imgStyle
+					if (img.align == "right") imgStyle = "float:right;" + imgStyle
+					if (img.parentElement.href) imgStyle = "cursor:hand;" + imgStyle
+					var strNewHTML = "<span " + imgID + imgClass + imgTitle
+					+ " style=\"" + "width:" + img.width + "px; height:" + img.height + "px;" + imgStyle + ";"
+					+ "filter:progid:DXImageTransform.Microsoft.AlphaImageLoader"
+					+ "(src=\'" + img.src + "\', sizingMethod='scale');\"></span>" 
+					img.outerHTML = strNewHTML
+					i = i-1
+				}
+			}
 		}
 	},		
 	language : function(language){
 		arr = new Array();
 		var title_intro, desc_intro, mooreinfo, title_splash, description_splash, invite_splash;
-			switch(language){
-				case 'pt':
-					title_intro = 'Seu navegador é antigo e possui sérias falhas de segurança.';
-					desc_intro = 'Atualize-o gratuitamente por um dos navegadores ao lado.';
-					mooreinfo = 'Saiba mais';
-					title_splash = 'Por que devo atualizar?';
-					description_splash = 'Os navegadores antigos, como o <strong>Internet Explorer 6</strong>, possuem sérios problemas de segurança e não são compatíveis com muitas funcionalidades dos sites atuais. Por isso, para ter uma melhor experiência de navegação e evitar riscos com vírus e programas espiões, baixe gratuitamente e instale no seu computador um dos navegadores modernos disponíveis.';
-					invite_splash = 'Atualize-o gratuitamente já!'
-					arr.push({title_intro:title_intro, desc_intro:desc_intro, mooreinfo:mooreinfo, title_splash:title_splash, description_splash:description_splash, invite_splash:invite_splash});
-				break;
-				case 'es':
-					title_intro = 'Su navegador es una versión vieja y tiene graves fallas de seguridad.';
-					desc_intro = 'Actualícelo gratuitamente con uno de los navegadores al lado.';
-					mooreinfo = 'Saber mas';
-					title_splash = '¿Por qué actualizar?';
-					description_splash = 'Las versiones anteriores de navegadores, como el <strong>Internet Explorer 6</strong>, presentan graves problemas de seguridad y son incompatibles con muchas funcionalidades de los sitios actuales. Por eso, para tener una experiencia de navegación mejor y evitar riesgos de virus y programas espías, descargue gratuitamente e instale en su computadora uno de los navegadores modernos disponibles.';
-					invite_splash = '¡Actualícelo ya gratuitamente!';
-					arr.push({title_intro:title_intro, desc_intro:desc_intro, mooreinfo:mooreinfo, title_splash:title_splash, description_splash:description_splash, invite_splash:invite_splash});
-				break;
-				case 'en':
-					title_intro = 'Your browser is old and has serious security flaws.';
-					desc_intro = 'Please upgrade it at no cost by using one of the browsers at the side.';
-					mooreinfo = 'More info';
-					title_splash = 'Why upgrade?';
-					description_splash = 'Old browsers such as <strong>Internet Explorer 6</strong> have serious security problems and are not compatible with many features of current websites. Therefore, to have a better browsing experience and to avoid risks with viruses and spyware, download and install on your computer at no cost one of these new browsers available.';
-					invite_splash = 'Upgrade for free now!';
-					arr.push({title_intro:title_intro, desc_intro:desc_intro, mooreinfo:mooreinfo, title_splash:title_splash, description_splash:description_splash, invite_splash:invite_splash});
-				break;
-			}
-			return arr;
+		switch(language){
+			case 'pt':
+			title_intro = 'Seu navegador é antigo e possui sérias falhas de segurança.';
+			desc_intro = 'Atualize-o gratuitamente por um dos navegadores ao lado.';
+			mooreinfo = 'Saiba mais';
+			title_splash = 'Por que devo atualizar?';
+			description_splash = 'Os navegadores antigos, como o <strong>Internet Explorer 6</strong>, possuem sérios problemas de segurança e não são compatíveis com muitas funcionalidades dos sites atuais. Por isso, para ter uma melhor experiência de navegação e evitar riscos com vírus e programas espiões, baixe gratuitamente e instale no seu computador um dos navegadores modernos disponíveis.';
+			invite_splash = 'Atualize-o gratuitamente já!'
+			arr.push({title_intro:title_intro, desc_intro:desc_intro, mooreinfo:mooreinfo, title_splash:title_splash, description_splash:description_splash, invite_splash:invite_splash});
+			break;
+			case 'es':
+			title_intro = 'Su navegador es una versión vieja y tiene graves fallas de seguridad.';
+			desc_intro = 'Actualícelo gratuitamente con uno de los navegadores al lado.';
+			mooreinfo = 'Saber mas';
+			title_splash = '¿Por qué actualizar?';
+			description_splash = 'Las versiones anteriores de navegadores, como el <strong>Internet Explorer 6</strong>, presentan graves problemas de seguridad y son incompatibles con muchas funcionalidades de los sitios actuales. Por eso, para tener una experiencia de navegación mejor y evitar riesgos de virus y programas espías, descargue gratuitamente e instale en su computadora uno de los navegadores modernos disponibles.';
+			invite_splash = '¡Actualícelo ya gratuitamente!';
+			arr.push({title_intro:title_intro, desc_intro:desc_intro, mooreinfo:mooreinfo, title_splash:title_splash, description_splash:description_splash, invite_splash:invite_splash});
+			break;
+			case 'en':
+			title_intro = 'Your browser is old and has serious security flaws.';
+			desc_intro = 'Please upgrade it at no cost by using one of the browsers at the side.';
+			mooreinfo = 'More info';
+			title_splash = 'Why upgrade?';
+			description_splash = 'Old browsers such as <strong>Internet Explorer 6</strong> have serious security problems and are not compatible with many features of current websites. Therefore, to have a better browsing experience and to avoid risks with viruses and spyware, download and install on your computer at no cost one of these new browsers available.';
+			invite_splash = 'Upgrade for free now!';
+			arr.push({title_intro:title_intro, desc_intro:desc_intro, mooreinfo:mooreinfo, title_splash:title_splash, description_splash:description_splash, invite_splash:invite_splash});
+			break;
+		}
+		return arr;
 	},
 
 	init : function(colorBg, colorText, lg) {
 
 		if (this.getCookie('deprecated_browser').length > 0) 
-			return false;
+		return false;
 
 		var arrLanguage = this.language(lg);
 		language = lg;
@@ -154,7 +154,7 @@ var deprecated_browser = {
 		if (document.createElement){
 			document.body.prependChild = function(newChild) {
 				this.insertBefore(newChild, this.firstChild);
-				};		
+			};		
 			var deprecated_browser = document.createElement('div');
 			deprecated_browser.setAttribute('id', 'deprecated_browser');
 			document.body.prependChild(deprecated_browser);	
@@ -181,7 +181,7 @@ var deprecated_browser = {
 				var k = rightSide.getElementsByTagName('a')[i];
 				var imageAlpha = k.firstChild;
 				imageAlpha.setAttribute('class', 'deprecated_browser_icob');
-				};
+			};
 			var closeButton = document.getElementById('close_deprecated');
 			var img = document.getElementById("right_deprecated").getElementsByTagName('img');
 			this.fix(img);
